59f36de8467b7174cf32b1a9f576844268794385
adamski-dev <adam.poplawski.programista@gmail.com>
2025-08-12 17:51:55 +0100

    Added client side walking skeleten in React

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,15 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+//adding CORS services
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
+    
 app.MapControllers();
 
 //Migration from the code:

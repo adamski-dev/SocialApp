5f97f89a9f0b1affaf8f97b57be461908e5bdc7b
adamski-dev <adam.poplawski.programista@gmail.com>
2025-08-17 20:15:30 +0100

    Moving IMediator to the BaseApiController class to be utilized by any Controller class derived from BaseApiController

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -10,19 +10,19 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     //we will use the primary constructor to access AbbDbContext class
-    public class ActivitiesController(IMediator mediator) : BaseApiController
+    public class ActivitiesController : BaseApiController
     {
         // We want to return Http Response so we use ActionResult of type of List of Activity
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await mediator.Send(new GetActivityList.Query());
+            return await Mediator.Send(new GetActivityList.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(string id)
         {
-            return await mediator.Send(new GetActivityDetails.Query {Id = id});
+            return await Mediator.Send(new GetActivityDetails.Query {Id = id});
         }
     }
 }

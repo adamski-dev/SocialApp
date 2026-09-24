872ae0d2624eadc6a7d032c6391c70b66a7fa1e4
adamski-dev <adam.poplawski.programista@gmail.com>
2026-09-24 20:12:40 +0100

    attendance feature added to client side

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -54,6 +54,7 @@ namespace API.Controllers
         [HttpPost("{id}/attend")]
         public async Task<ActionResult>Attend(string id)
         {
+            return BadRequest();
             return HandleResult(await Mediator.Send(new UpdateAttendance.Command {Id = id}));
         }
     }
